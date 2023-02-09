f6a909969deed51f45d37ec503fc401b257406a2
fracs31 <pist.fracs95@gmail.com>
2023-02-09 10:16:42 +0100

    FIX the isPalindrome function using split, reverse and join

--- js/script.js
+++ js/script.js
@@ -39,21 +39,11 @@ switch (exercise) {
 
 //Funzione per capire se una parola è palindroma
 function isPalindrome(word) {
-    let letters = word.split(""); //array che contiene ogni singola lettera della parola inserita dall'utente
-    let reverse = []; //array che contiene in ordine opposto ogni singola lettera della parola inserita dall'utente
+    let reverse = word.split("").reverse().join(""); //parola al contrario
     let result = false; //variabile che controlla se la parola inserita è palindroma
-    //Ciclo
-    for (let i = (letters.length - 1); i >= 0; i--) {
-        reverse.push(letters[i]); //inserisco ogni lettera della parola in ordine opposto nell'array
-    }
-    //Ciclo
-    for (let i = 0; i < letters.length; i++) {
-        //Se le lettere coincidono in ogni posizione degli array
-        if (letters[i] == reverse[i]) {
-            result = true; //la parola è palindroma
-        } else { //altrimenti
-            result = false; //la parola non è palindroma
-        }
+    //Se la parola inserita è uguale alla parola al contrario
+    if (word == reverse) {
+        result = true; //la parola inserita è palindroma
     }
     return result; //restituisco il risultato
 }
