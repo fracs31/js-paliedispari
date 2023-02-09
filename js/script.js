910393687bd657fd248bf55822af61f1e4044e8c
fracs31 <pist.fracs95@gmail.com>
2023-02-09 10:06:38 +0100

    CHANGE palidrome word method using the split() function

--- js/script.js
+++ js/script.js
@@ -39,13 +39,9 @@ switch (exercise) {
 
 //Funzione per capire se una parola è palindroma
 function isPalindrome(word) {
-    let letters = []; //array che contiene ogni singola lettera della parola inserita dall'utente
+    let letters = word.split(""); //array che contiene ogni singola lettera della parola inserita dall'utente
     let reverse = []; //array che contiene in ordine opposto ogni singola lettera della parola inserita dall'utente
     let result = false; //variabile che controlla se la parola inserita è palindroma
-    //Ciclo
-    for (let i = 0; i < word.length; i++) {
-        letters.push(word.charAt(i)); //prendo ogni lettera della parola e la inserisco nell'array
-    }
     //Ciclo
     for (let i = (letters.length - 1); i >= 0; i--) {
         reverse.push(letters[i]); //inserisco ogni lettera della parola in ordine opposto nell'array
